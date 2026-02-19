87e9c15eda318c73bdfe98ea103b8969b147c4d9
Nimeshan <nimeshan@example.com>
2026-02-19 10:36:53 +0530

    Fix: Add FaArrowRight back to imports - still used in email form

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { FaArrowUp, FaHeart } from "react-icons/fa";
+import { FaArrowUp, FaArrowRight, FaHeart } from "react-icons/fa";
 
 // Right arrow icon (matching ServiceFooterNav style)
 const RightArrowIcon: React.FC = () => (
