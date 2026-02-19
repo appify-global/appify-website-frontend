003efac1af6417c1fc03cd45cf0c2af6dda972f2
Nimeshan <nimeshan@example.com>
2026-02-19 12:06:10 +0530

    Add plus icons separator between Clients and Talks sections

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -39,6 +39,32 @@ export default function AboutPage() {
         <ClientsSection />
       </section>
 
+      {/* Plus Icons Separator */}
+      <section className="w-full bg-black py-[40px] lg:py-[60px]">
+        <div className="px-[4vw] sm:px-[6vw] lg:px-[5vw]">
+          <div className="flex items-center justify-between w-full">
+            {[0, 1, 2, 3].map((i) => (
+              <svg
+                key={i}
+                width="21"
+                height="21"
+                viewBox="0 0 22 22"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-4 h-4 lg:w-5 lg:h-5 text-white/60"
+              >
+                <path
+                  d="M11.7581 0.149597V9.84198H21.4504V11.758H11.7581V21.4504H9.84204V11.758H0.149658V9.84198H9.84204V0.149597H11.7581Z"
+                  fill="currentColor"
+                  stroke="currentColor"
+                  strokeWidth="0.3"
+                />
+              </svg>
+            ))}
+          </div>
+        </div>
+      </section>
+
       {/* Awards Section (awards table hidden, articles and talks visible) */}
       <section id="awards" className="w-full">
         <AwardsSection />
