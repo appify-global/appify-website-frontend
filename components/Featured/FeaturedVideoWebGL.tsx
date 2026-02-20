52e264dfcd694bb5c8973b2b6f1c74f5c714f049
Nimeshan <nimeshan@example.com>
2026-02-20 17:11:45 +0530

    Move reel container down slightly

--- components/Featured/FeaturedVideoWebGL.tsx
+++ components/Featured/FeaturedVideoWebGL.tsx
@@ -326,7 +326,7 @@ const FeaturedVideoWebGL = ({
         ref={reelContainerRef}
         className="absolute left-1/2 -translate-x-1/2 pointer-events-none"
         style={{
-          top: "40vh", // Position moved up
+          top: "45vh", // Position moved down slightly
           width: "85vw",
           aspectRatio: "2.1 / 1",
           opacity: 0,
