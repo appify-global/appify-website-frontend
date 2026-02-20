c2dae77d56088a7fc3fa6c9d317004b9c03e9e8a
Nimeshan <nimeshan@example.com>
2026-02-20 15:00:12 +0530

    Update reel container reference width to 85vw

--- components/Featured/FeaturedVideoWebGL.tsx
+++ components/Featured/FeaturedVideoWebGL.tsx
@@ -211,7 +211,7 @@ const FeaturedVideoWebGL = ({
         ref={reelContainerRef}
         className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none"
         style={{
-          width: "75vw",
+          width: "85vw",
           aspectRatio: "2.1 / 1",
           opacity: 0,
           zIndex: -1,
